5c9ac39b0cf745a5c943d15602560c41ae46f751
Adityakr152 <edugaonaditya@gmail.com>
2025-10-22 14:45:09 +0530

    aditya add a aditya file

---
+++ lib/aditya.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class Aditya extends StatefulWidget {
+  const Aditya({super.key});
+
+  @override
+  State<Aditya> createState() => _AdityaState();
+}
+
+class _AdityaState extends State<Aditya> {
+  @override
+  Widget build(BuildContext context) {
+    return Scafold(
+
+    );
+  }
+}
