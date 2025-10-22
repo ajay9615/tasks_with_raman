7b8ead1a80ad1a75200d537c04ee321d1ce10692
hp <pujakumarirai109@gmail.com>
2025-10-22 15:04:54 +0530

    Merge remote-tracking branch 'origin/poojas' into poojas

--- lib/aditya.dart
+++ lib/aditya.dart
@@ -9,9 +9,7 @@ class Aditya extends StatefulWidget {
 class _AdityaState extends State<Aditya> {
   @override
   Widget build(BuildContext context) {
-    return Scafold(
-      app
-
-    );
+    return Scaffold( appBar: AppBar(backgroundColor: Colors.red,),)
+;
   }
 }
