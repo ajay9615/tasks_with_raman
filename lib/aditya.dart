5e27885aa55b6cfa1722ccd1f3e8b63a58abb489
nagmakhatoon0011 <nagmaedugaon786@gmail.com>
2025-10-22 22:04:01 +0530

    2 changed in this app

--- lib/aditya.dart
+++ lib/aditya.dart
@@ -11,7 +11,7 @@ class _AdityaState extends State<Aditya> {
   Widget build(BuildContext context) {
     return  Scaffold(
       body: Center(
-        child: Text("R"),
+        child: Text("aditya")
       ),
     )
     ;
